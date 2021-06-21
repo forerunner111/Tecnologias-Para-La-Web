915eb29215e535b338e6789495652b40064d41b8
forerunner111 <58716959+forerunner111@users.noreply.github.com>
2021-06-21 09:17:49 -0500

    Add files via upload

---
+++ Departamental2/js/ejercicio-10.js
@@ -0,0 +1,21 @@
+function mostrarInfo(){
+    $('#div-info').toggle(500); 
+}
+function mostrarPrecaucion(){
+    $('#div-preca').toggle(500);
+}
+function mostrarsuccess(){
+    $('#div-success').toggle(500);
+}
+function mostrarpeligro(){
+    $('#div-Danger').toggle(500);
+}
+function mostrarprimary(){
+    $('#div-primary').toggle(500);
+}
+function mostrarsecondary(){
+    $('#div-secondary').toggle(500);
+}
+function mostrardefault(){
+    $('#div-default').toggle(500);
+}
